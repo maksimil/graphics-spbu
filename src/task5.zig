da9e2c00d74c6247726d1a68e93b0098d8865fdb
Maxim Kosterov <maxim.kosterov@gmail.com>
2025-04-23 20:27:00 +0300

    simple iterative method for task 5

--- src/task5.zig
+++ src/task5.zig
@@ -74,8 +74,59 @@ const TileRasterizer = struct {
     }
 };
 
-// fn SimpleIterative(r: TileRasterizer, x0: i32, y0: i32) !void {
-// }
+fn SimpleIterative(r: TileRasterizer, x0: i32, y0: i32) !void {
+    const inner_color = r.GetPx(x0, y0).*;
+    const fill_color = raster.RGBA_RED;
+
+    var neighbours_list = std.ArrayList([2]i32).init(config.allocator);
+    defer neighbours_list.deinit();
+
+    try neighbours_list.append(.{ x0, y0 });
+    r.DrawPx(x0, y0, fill_color);
+
+    const max_size = 1024;
+    var idx: usize = 0;
+
+    var k: usize = 0;
+    while (neighbours_list.items.len > idx) {
+        config.stdout.print("k={any}, len={any}\n", .{ k, neighbours_list.items.len }) catch unreachable;
+        k += 1;
+
+        const size = neighbours_list.items.len - idx;
+        for (0..size) |i| {
+            const p = neighbours_list.items[idx + i];
+            const x = p[0];
+            const y = p[1];
+            std.debug.assert(std.meta.eql(r.GetPx(x, y).*, fill_color));
+            r.DrawPx(x, y, raster.RGBA_RED);
+
+            for (0..4) |j| {
+                const x1 = x +
+                    @as(i32, @intFromBool(j == 0)) -
+                    @as(i32, @intFromBool(j == 2));
+                const y1 = y +
+                    @as(i32, @intFromBool(j == 1)) -
+                    @as(i32, @intFromBool(j == 3));
+
+                if (x1 >= 0 and x1 < kTileWidth and
+                    y1 >= 0 and y1 < kTileHeight and
+                    std.meta.eql(r.GetPx(x1, y1).*, inner_color))
+                {
+                    try neighbours_list.append(.{ x1, y1 });
+                    r.DrawPx(x1, y1, fill_color);
+                }
+            }
+        }
+
+        if (neighbours_list.items.len >= max_size) {
+            const range: [0][2]i32 = .{};
+            try neighbours_list.replaceRange(0, idx + size, &range);
+            idx = 0;
+        } else {
+            idx = idx + size;
+        }
+    }
+}
 
 pub fn Run() !void {
     const raster_data = try config.allocator.alloc(
@@ -146,6 +197,8 @@ pub fn Run() !void {
             );
         }
     }
+
+    try SimpleIterative(tile0, 200, 200);
 
     try r.RenderOut("out.png");
 }
